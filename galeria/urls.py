b1d963cdc1ed1c4fcac9217fb3a355741c49d8b9
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-22 23:38:03 -0300

    vinculando a segunda pagina

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from galeria.views import index
+from galeria.views import index, personagem
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('personagem/', personagem)
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'galeria/index.html')
+
+def personagem(request):
+    return render(request, 'galeria/personagem.html')
