4484c8207c8cf10ebe136315489cf9c2e86e4a7f
RenanFumis <renan-oliveira19@hotmail.com>
2024-05-24 00:08:47 -0300

    inicializando os contatos com o banco de dados integrado

--- galeria/urls.py
+++ galeria/urls.py
@@ -3,7 +3,8 @@ from galeria.views import index, personagem, herois
 
 urlpatterns = [
     path('', index, name='index'),
-    path('personagem/', personagem, name='personagem'),
-    path('herois/', herois, name='herois')
+    path('herois/', herois, name='herois'),
+
+    path('personagem/', personagem, name='personagem')
 
 ]
--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from galeria.models import Imagem
 
 
 def index(request):
@@ -8,4 +9,16 @@ def personagem(request):
     return render(request, 'galeria/personagem.html')
 
 def herois(request):
-    return render(request, 'galeria/herois.html')
+    # dados={
+    #     2: {
+    #         "nome": "Keiko Yukimura",
+    #         "codinome": "A Namorada"
+    #     },
+    #     3: {
+    #         "nome": "Kazuma Kuwabara",
+    #         "codinome": "O Espadachim"
+    #     },
+        
+    #     }
+    fotos = Imagem.objects.all()
+    return render(request, 'galeria/herois.html', {'personagem': fotos})
